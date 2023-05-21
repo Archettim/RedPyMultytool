147f94528379381fefa5769b73f0ed9beb5e6652
Archettim <Mikyarchetti@gmail.com>
2023-05-21 02:48:55 +0200

    ArpPoisoning first implement Get_target_mac()

--- ArpPoisoning.py
+++ ArpPoisoning.py
@@ -1,8 +1,16 @@
 import os
-import textual
-from scapy.all import (ARP, Ether, conf, get_if_hwaddr,
-send, sniff, sndrcv, srp, wrpcap)
+import sys
+from scapy.all import (ARP, Ether, conf, get_if_hwaddr,send, sniff, sndrcv, srp, wrpcap)
+
+def get_target_mac(ip):
+    packet=Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(op="who-has",pdst=ip)
+    resp,_=srp(packet,timeout=2,retry=10,verbose=False)
+    for _, r in resp:
+        return(r[Ether].src)
 
 class ArpPoison:
     def __init__(self) -> None:
         pass
+
+if __name__=="__main__":
+    print(get_target_mac(input("ip:")))
